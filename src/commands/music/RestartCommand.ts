07daf343e499aa9d65c071daa54aa4c200c27819
Pizzarous <angelocavaqueira@gmail.com>
2023-02-03 21:04:31 +0000

    new command: restart / language adjustments

---
+++ src/commands/music/RestartCommand.ts
@@ -0,0 +1,35 @@
+import { CommandContext } from "../../structures/CommandContext";
+import { createEmbed } from "../../utils/functions/createEmbed";
+import { BaseCommand } from "../../structures/BaseCommand";
+import { Command } from "../../utils/decorators/Command";
+import i18n from "../../config";
+
+@Command({
+    aliases: ["restart", "reboot", "reset"],
+    description: i18n.__("commands.music.restart.description"),
+    name: "restart",
+    slash: {
+        options: []
+    },
+    usage: "{prefix}restart"
+})
+export class RestartCommand extends BaseCommand {
+    public execute(ctx: CommandContext): void {
+        if ((!process.env.ADMIN_ID) || ctx.author.id !== process.env.ADMIN_ID) {
+            ctx.reply({
+                embeds: [createEmbed("error", `❌ **|** ${i18n.__("commands.music.restart.errorMessage")}`)]
+            }).catch(e => this.client.logger.error("RESTART_CMD_ERR:", e));
+            return;
+        }
+
+        ctx.guild?.queue?.destroy();
+
+        ctx.reply({
+            embeds: [createEmbed("success", `👋 **|** ${i18n.__("commands.music.restart.leftMessage")}`)]
+        }).catch(e => this.client.logger.error("RESTART_CMD_ERR:", e));
+
+        setTimeout(() => {
+            process.exit(0);
+        }, 2000);
+    }
+}
