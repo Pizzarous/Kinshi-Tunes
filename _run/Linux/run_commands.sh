8b3cad22510c744b8c5bfcf7efa5ebf206539cf1
Pizzarous <angelocavaqueira@gmail.com>
2024-01-20 22:25:53 +0000

    -

--- _run/Linux/run_commands.sh
+++ _run/Linux/run_commands.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cd ..
-cd ..
+# Get the directory where the script is located
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to the grandparent directory of the script
+cd "$script_dir"/../../
+
+# Continue with the rest of your commands
 git reset --hard origin/main
 git pull --all
 npm install
